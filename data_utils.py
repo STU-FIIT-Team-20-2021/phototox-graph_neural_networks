changed rdkit feature scaling, added valence, fixed wandb logging

data_utils.py
@@ -1,4 +1,5 @@
 from rdkit import Chem
+from rdkit.Chem import AllChem
 from rdkit.Chem.rdmolops import GetAdjacencyMatrix
 import torch
 import numpy as np
@@ -29,13 +30,18 @@ def get_atom_features(atom,
     formal_charge_enc = one_hot_encoding(int(atom.GetFormalCharge()), [-3, -2, -1, 0, 1, 2, 3, "Extreme"])
     hybridisation_type_enc = one_hot_encoding(str(atom.GetHybridization()),
                                               ["S", "SP", "SP2", "SP3", "SP3D", "SP3D2", "OTHER"])
+
     is_in_a_ring_enc = [int(atom.IsInRing())]
     is_aromatic_enc = [int(atom.GetIsAromatic())]
-    atomic_mass_scaled = [float((atom.GetMass() - 10.812) / 116.092)]
-    vdw_radius_scaled = [float((Chem.GetPeriodicTable().GetRvdw(atom.GetAtomicNum()) - 1.5) / 0.6)]
-    covalent_radius_scaled = [float((Chem.GetPeriodicTable().GetRcovalent(atom.GetAtomicNum()) - 0.64) / 0.76)]
+    atomic_mass_scaled = [np.log(float((atom.GetMass())))]
+    vdw_radius_scaled = [float((Chem.GetPeriodicTable().GetRvdw(atom.GetAtomicNum())))]
+    covalent_radius_scaled = [float((Chem.GetPeriodicTable().GetRcovalent(atom.GetAtomicNum())))]
+    # gasteiger_charge = [float(atom.GetProp("_GasteigerCharge"))]
+    explicit_valence = [float(atom.GetExplicitValence())]
 
-    atom_feature_vector = atom_type_enc + n_heavy_neighbors_enc + formal_charge_enc + hybridisation_type_enc + is_in_a_ring_enc + is_aromatic_enc + atomic_mass_scaled + vdw_radius_scaled + covalent_radius_scaled
+    atom_feature_vector = atom_type_enc + n_heavy_neighbors_enc + formal_charge_enc + hybridisation_type_enc + \
+                          is_in_a_ring_enc + is_aromatic_enc + atomic_mass_scaled + vdw_radius_scaled + \
+                          covalent_radius_scaled + explicit_valence
 
     if use_chirality:
         chirality_type_enc = one_hot_encoding(str(atom.GetChiralTag()),
@@ -51,7 +57,7 @@ def get_atom_features(atom,
 
 
 def get_bond_features(bond,
-                      use_stereochemistry=True):
+                      use_stereochemistry=False):
     permitted_list_of_bond_types = [Chem.rdchem.BondType.SINGLE, Chem.rdchem.BondType.DOUBLE,
                                     Chem.rdchem.BondType.TRIPLE, Chem.rdchem.BondType.AROMATIC]
     bond_type_enc = one_hot_encoding(bond.GetBondType(), permitted_list_of_bond_types)
@@ -82,6 +88,7 @@ def create_data_list(x_smiles, y):
 
         X = np.zeros((n_nodes, n_node_features))
 
+        AllChem.ComputeGasteigerCharges(mol)
         for atom in mol.GetAtoms():
             X[atom.GetIdx(), :] = get_atom_features(atom)
 

main.py
@@ -46,17 +46,17 @@ config_default = {
     'c_hidden_hard': 256,
     'optim': "Adam",
     'type': 'GAT',
-    'pos_weight': 1.5,
+    'pos_weight': 1,
     'lr': 1e-3,
     'batch_size': 1024
 }
 
-# setup_training(config_default, './outputs/test_run', pre_df, wandb_name='test_run')
-
+setup_training(config_default, './outputs/test_run', pre_df, wandb_name='test_run', pretrain=True)
+wandb.finish()
 pre_trained = glob.glob('./outputs/test_run/*.pth')
 last = max(pre_trained, key=os.path.getctime)
 device = torch.device("cuda:0") if torch.cuda.is_available() else torch.device("cpu")
-net = GraphGNNModel(79, config_default['c_hidden_soft'], config_default['c_hidden_soft'], dp_rate_linear=config_default['drop_rate_soft_dense'],
+net = GraphGNNModel(80, config_default['c_hidden_soft'], config_default['c_hidden_soft'], dp_rate_linear=config_default['drop_rate_soft_dense'],
                             dp_gnn=config_default['drop_rate_soft'],**config_default)
 net.load_state_dict(torch.load(last, map_location=device))
 

train.py
@@ -21,10 +21,10 @@ from sklearn.metrics import classification_report
 from sklearn.model_selection import train_test_split
 
 
-def setup_training(config: dict, save_checkpoint: str, df: pd.DataFrame, wandb_name: str, net: nn.Module = None):
+def setup_training(config: dict, save_checkpoint: str, df: pd.DataFrame, wandb_name: str, net: nn.Module = None, pretrain: bool = False):
     device = torch.device("cuda:0") if torch.cuda.is_available() else torch.device("cpu")
     if net is None:
-        net = GraphGNNModel(79, config['c_hidden_soft'], config['c_hidden_soft'], dp_rate_linear=config['drop_rate_soft_dense'],
+        net = GraphGNNModel(80, config['c_hidden_soft'], config['c_hidden_soft'], dp_rate_linear=config['drop_rate_soft_dense'],
                             dp_gnn=config['drop_rate_soft'],**config)
     net.to(device)
 
@@ -40,7 +40,8 @@ def setup_training(config: dict, save_checkpoint: str, df: pd.DataFrame, wandb_n
                             })
 
     train_net(net, device, epochs=250, batch_size=config['batch_size'], learning_rate=config['lr'], experiment=experiment,
-              df=df, opt=config['optim'], dir_checkpoint=f'./outputs/{wandb_name}', pos_weight=config['pos_weight'])
+              df=df, opt=config['optim'], dir_checkpoint=f'./outputs/{wandb_name}', pos_weight=config['pos_weight'],
+              pretrain=pretrain)
 
 
 def train_net(net,
@@ -56,6 +57,7 @@ def train_net(net,
               experiment = None,
               pos_weight: float = 1.0,
               early_stopping_patience: int = 5,
+              pretrain: bool = False,
               l1_lambda: float = 0):
 
     logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(message)s')
@@ -123,7 +125,8 @@ def train_net(net,
                     if net.n_classes == 1:
                         pred = pred.squeeze(dim=-1)
 
-                    pred.requires_grad = True
+                    if not pretrain:
+                        pred.requires_grad = True
                     loss = criterion(pred, batch.y)
 
 